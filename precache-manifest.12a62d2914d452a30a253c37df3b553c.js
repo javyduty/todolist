522d0bba1111685c7de565d11d31187bafe4d90a
javyduty <javydutyalante@gmail.com>
2020-01-02 22:23:36 +0900

    Updates

---
+++ precache-manifest.12a62d2914d452a30a253c37df3b553c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb316fa2b515257bbf427ff5db17fa7",
+    "url": "/javyduty/todolist/index.html"
+  },
+  {
+    "revision": "a976e43c54167183e67a",
+    "url": "/javyduty/todolist/static/css/2.0b9fb118.chunk.css"
+  },
+  {
+    "revision": "34dce5418f954fc2bd72",
+    "url": "/javyduty/todolist/static/css/main.fcf9c214.chunk.css"
+  },
+  {
+    "revision": "a976e43c54167183e67a",
+    "url": "/javyduty/todolist/static/js/2.a36fa530.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/javyduty/todolist/static/js/2.a36fa530.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34dce5418f954fc2bd72",
+    "url": "/javyduty/todolist/static/js/main.4484ad7d.chunk.js"
+  },
+  {
+    "revision": "516de71113ca1bf17aa3",
+    "url": "/javyduty/todolist/static/js/runtime-main.c2102909.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/javyduty/todolist/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/javyduty/todolist/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/javyduty/todolist/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/javyduty/todolist/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/javyduty/todolist/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
